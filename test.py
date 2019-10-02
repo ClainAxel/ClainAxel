6e2c3ecf32fddb91dbd0cba6628222c8a297525b
isa <sigtig.isa@gmail.com>
2019-10-02 17:14:59 +0200

    avecfct

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-verife=true
-if verife:
-          print("bravo super")
+def taille:
+	verif=true
+	if verif:
+        	 print("bravo super")
+	nom='John'
+	prenom = input("quel est ton prenom ?")
+	if len(prenom) >7:
+		print("beaucoup de lettres")
+	else:
+		print ("prenom rapide à écrire")
+	return   verif
+print  (taille)
+taval=input("entre un chiffre")
+print (taval)
+
